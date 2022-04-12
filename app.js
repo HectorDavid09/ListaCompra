dfbc165f5230bc247d877f0e0d5d4e4b4a2a68c1
HectorDavid09 <101266985+HectorDavid09@users.noreply.github.com>
2022-04-12 17:03:02 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,92 @@
+const formularioUI = document.querySelector('#formulario');
+const listaProductosUI = document.getElementById('listaProductos');
+let arrayProductos = [];
+
+
+//---------------------------
+
+const CrearItem = (producto) =>{
+
+    let item = {
+        producto: producto,
+        estado: false
+    };
+
+    arrayProductos.push(item);
+    return item;
+}
+
+const GuardarDB = (producto) =>{
+
+    localStorage.setItem('compra',JSON.stringify(arrayProductos))
+    PintarDB();
+
+}
+
+const PintarDB =()=>{
+
+    listaProductosUI.innerHTML='';
+
+    arrayProductos = JSON.parse(localStorage.getItem('compra'));
+
+    if(arrayProductos === null){
+        arrayProductos=[];
+    }else{
+        arrayProductos.forEach(element => {
+            if(element.estado){
+                listaProductosUI.innerHTML += `<div class="alert alert-primary" role="alert"><i class="material-icons float-left mr-4">add_shopping_cart</i><b> ${element.producto} </b> - ${element.estado}<span class="float-right"><i class="material-icons">done</i><i class="material-icons">delete_outline</i></span></div>`
+
+            }else{
+                listaProductosUI.innerHTML += `<div class="alert alert-danger" role="alert"><i class="material-icons float-left mr-4">add_shopping_cart</i><b> ${element.producto} </b> - ${element.estado}<span class="float-right"><i class="material-icons">done</i><i class="material-icons">delete_outline</i></span></div>`
+
+            }
+        });
+    }
+}
+
+const EliminarDB = (producto) =>{
+    let indexArray;
+    arrayProductos.forEach((elemento, index) => {
+        if(elemento.producto === producto){
+            indexArray = index;
+        }
+    });
+
+    arrayProductos.splice(indexArray,1);
+    GuardarDB();
+}
+
+const EditarDB = (producto) => {
+    let indexArray = arrayProductos.findIndex((elemento) => elemento.producto == producto);
+    arrayProductos[indexArray].estado = 'true';
+    GuardarDB();
+}
+
+//------------------------------
+
+formularioUI.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    let productoUI = document.querySelector('#producto').value;
+    
+    CrearItem(productoUI);
+    GuardarDB();
+    formularioUI.reset();
+});
+
+document.addEventListener('DOMContentLoaded', PintarDB);
+
+listaProductosUI.addEventListener('click', (e)=>{
+    e.preventDefault();
+    if((e.target.innerHTML === 'done' || e.target.innerHTML === 'delete_outline')){
+            let texto = e.path[2].childNodes[1].innerHTML;
+            if(e.target.innerHTML === 'delete_outline'){
+                EliminarDB(texto);
+            }
+            
+            if(e.target.innerHTML === 'done'){
+                EditarDB(texto);
+            }
+    }
+});
+
+
